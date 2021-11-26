def32905b95d8201f4a0cd40f8e2a9ed9c0f5dc8
Heansum <heansum1556@gmail.com>
2021-11-26 13:21:25 +0900

    Implementation of memset

--- ft_memset_test.c
+++ ft_memset_test.c
@@ -6,11 +6,32 @@
 /*   By: hlim <hlim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 18:17:19 by hlim              #+#    #+#             */
-/*   Updated: 2021/11/23 18:46:28 by hlim             ###   ########.fr       */
+/*   Updated: 2021/11/26 13:19:43 by hlim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void* memset(void* ptr, int value, size_t num)
-{
+#include <stdio.h>
 
+void	*ft_memset(void *ptr, int value, unsigned int num)
+{
+	unsigned char	data;
+	unsigned char	*start_ptr;
+
+	start_ptr = (unsigned char *)ptr;
+	data = (unsigned char)value;
+	while (num-- > 0)
+		*start_ptr++ = data;
+	return (ptr);
+}
+
+int main(void)
+{
+	char str[11] = "Hello World";
+	void *void_str;
+
+	printf("%s\n", str);
+	ft_memset(str, '1', 4);
+	printf("%s\n", str);
+
+	return (0);
 }
