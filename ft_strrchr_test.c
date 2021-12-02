dc22a7e198c35ba8d7cec75f5c7506268571da42
Heansum <heansum1556@gmail.com>
2021-12-02 13:04:38 +0900

    Implementation of strrchr

--- ft_strrchr_test.c
+++ ft_strrchr_test.c
@@ -1,40 +1,55 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr_test.c                                   :+:      :+:    :+:   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hlim <hlim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/02 11:59:01 by hlim              #+#    #+#             */
-/*   Updated: 2021/12/02 12:32:13 by hlim             ###   ########.fr       */
+/*   Created: 2021/12/02 12:32:43 by hlim              #+#    #+#             */
+/*   Updated: 2021/12/02 13:03:03 by hlim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	*ft_strchr(char *str, char c)
+int	ft_strlen(const char *str)
+{
+	int	nb;
+
+	nb = 0;
+	while (*str++)
+		nb++;
+	return (nb);
+}
+
+const char	*ft_strrchr(const char *str, int ch)
 {
 	char	*ptr;
+	int	len;
 
-	ptr = str;
-	while (ptr != NULL)
+	ptr = (char *)str;
+	len = ft_strlen(ptr);
+	while (len)
 	{
-		if (ptr[0] == c)
-			return (ptr);
-		ptr++;
+		if (ptr[len] == ch)
+			return (ptr + len);
+		len--;
 	}
 	return (NULL);
 }
 
 int	main(void)
 {
-	char str[11] = "Hello World";
-	char *ptr;
-	
-	ptr = str;
-	printf("%s", ptr);
-	ptr = ft_strchr(str, 'o');
-	printf("%s", ptr);
-	
+	char	str[11] = "Hello World";
+	int	nb;
+	const char	*ptr;
+
+	nb = ft_strlen(str); 
+	printf("%d\n", nb);
+	printf("%s\n", str);
+	ptr = ft_strrchr(str, 'o');
+	printf("%s\n", ptr);	
+
 	return (0);
+}
 
