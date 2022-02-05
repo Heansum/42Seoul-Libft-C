178768ba8e470a6ce17a857b49e69014fa8df096
Heansum <heansum1556@gmail.com>
2022-02-06 01:11:42 +0900

    Modified strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: hlim <hlim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/28 15:25:56 by hlim              #+#    #+#             */
-/*   Updated: 2022/01/28 17:49:26 by hlim             ###   ########.fr       */
+/*   Updated: 2022/02/06 01:04:53 by hlim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,18 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t	i;
 	size_t	j;
-	size_t	big_len;
-	size_t	little_len;
 
 	i = 0;
-	big_len = ft_strlen(big);
-	little_len = ft_strlen(little);
 	if (little[i] == 0)
 		return ((char *)big);
-	while (big[i] != 0 || i < len)
+	while (big[i] != 0 && i < len)
 	{
 		j = 0;
-		while (big[i + j] == little[j] || big_len != len)
+		while (big[i + j] == little[j] && i + j < len)
 		{
-			if (j == little_len)
-				return ((char *)&big[i]);
 			j++;
+			if (little[j] == 0)
+				return ((char *)&big[i]);
 		}
 		i++;
 	}
