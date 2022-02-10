ef43857efe10321894e95e31dba8a344753a1987
Heansum <heansum1556@gmail.com>
2022-02-10 11:08:01 +0900

    Complete implementation of atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,17 +6,19 @@
 /*   By: hlim <hlim@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 19:35:40 by hlim              #+#    #+#             */
-/*   Updated: 2021/12/11 20:04:15 by hlim             ###   ########.fr       */
+/*   Updated: 2022/02/10 11:01:38 by hlim             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(const char *str)
 {
-	int	sign;
-	int	result;
+	unsigned long long	result;
+	unsigned long long	max;
+	int					sign;
 
 	sign = 1;
 	result = 0;
+	max = 9223372036854775807;
 	while ((9 <= *str && *str <= 13) || *str == ' ')
 		str++;
 	if (*str == '-' || *str == '+')
@@ -30,5 +32,9 @@ int	ft_atoi(const char *str)
 		result = (result * 10) + (*str - '0');
 		str++;
 	}
+	if (result > max && sign == 1)
+		return (-1);
+	else if (result > max + 1 && sign == -1)
+		return (0);
 	return (result * sign);
 }
